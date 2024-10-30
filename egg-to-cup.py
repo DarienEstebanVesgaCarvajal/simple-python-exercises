5af2e0bba7beb04c632089a7308e1b0446103994
DarienEstebanVesgaCarvajal <182381184+DarienEstebanVesgaCarvajal@users.noreply.github.com>
2024-10-30 11:29:50 -0500

    Egg-to-cup arrangement to understand

--- egg-to-cup.py
+++ egg-to-cup.py
@@ -1,17 +1,16 @@
-massAverage = 57
-density = 1.038
-specificHeat = 3.7
-thermalConductivity = 5.4e-3
-boilingTemp = 100
-yolkMaxTemp = 70
+massAverage = 57  # g
+density = 1.038  # g/cm^3
+specificHeat = 3.7  # J/g°C
+thermalConductivity = 5.4e-3  # W/cm°C
+boilingTemp = 100  # °C
+yolkMaxTemp = 70  # °C
 
 originalTemp = float(input("What's the original temperature of the egg? (°C): "))
 
-def log(x):
-    return (x - 1) - (x - 1)**2 / 2 + (x - 1)**3 / 3 - (x - 1)**4 / 4 + (x - 1)**5 / 5
+logVolumen = (0.76 * originalTemp - boilingTemp) / (yolkMaxTemp - boilingTemp)
 
-time = (massAverage ** (2/3) * specificHeat * density ** (1/3) * thermalConductivity * 
-        3.14159 ** 2 * (4 * 3.14159 / 3) ** (2/3) *
-        log((0.76 * originalTemp - boilingTemp) / (yolkMaxTemp - boilingTemp)))
+taylorSeries = (logVolumen - 1) - (logVolumen - 1)**2 / 2 + (logVolumen - 1)**3 / 3 - (logVolumen - 1)**4 / 4 + (logVolumen - 1)**5 / 5
+
+time = (massAverage ** (2 / 3) * specificHeat * density ** (1 / 3)) / (thermalConductivity * 3.1416 ** 2) * (1 + taylorSeries)
 
 print(f"The time needed in seconds to reach the maximum temperature is: {time}.")
