4a75b763038fc0cc8b9eeef1fedc2234293d7f75
DarienEstebanVesgaCarvajal <182381184+DarienEstebanVesgaCarvajal@users.noreply.github.com>
2024-10-30 11:47:35 -0500

    Improved user terminal content

--- request-note.py
+++ request-note.py
@@ -1,5 +1,5 @@
-noteFirstContest = float(input("What's the note of the first cerxam?: "))
-noteSecondContest = float(input("What's the note of the second cerxam?: "))
+noteFirstContest = float(input("What's the note of the first contest?: "))
+noteSecondContest = float(input("What's the note of the second contest?: "))
 noteLab = float(input("What's the note of lab?: "))
 
 desiredFinalNote = 60
@@ -8,4 +8,4 @@ requiredAverage = (desiredFinalNote - (noteLab * 0.3)) / 0.7
 
 noteThirdContest = (requiredAverage * 3) - (noteFirstContest + noteSecondContest)
 
-print(f"The note required to pass is: {noteThirdContest}")
+print(f"The note required in the third contest to pass is: {noteThirdContest}")
