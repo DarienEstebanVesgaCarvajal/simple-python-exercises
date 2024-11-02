0f53784df342615316874707390b26f40170ae38
DarienEstebanVesgaCarvajal <182381184+DarienEstebanVesgaCarvajal@users.noreply.github.com>
2024-11-02 14:54:47 -0500

    warm-egg

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-noteFirstContest = float(input("What's the note of the first contest?: "))
-noteSecondContest = float(input("What's the note of the second contest?: "))
-noteLab = float(input("What's the note of lab?: "))
+import math 
 
-desiredFinalNote = 60
+originalTemp = float(input("What's the original egg temperature?: "))
+boilingTemp = 100 
+yolkMaxTemp = 70  
+massAverage = 57 
+density = 1.038 
+specificHeat = 3.7
+thermalConductivity = 5.4 * math.pow(10, -3)
 
-requiredAverage = (desiredFinalNote - (noteLab * 0.3)) / 0.7
+dividend = math.pow(massAverage, (2/3)) * (specificHeat * (math.pow(density, (1/3))))
+divider = (thermalConductivity * math.pow(math.pi, 2)) * math.pow((4*math.pi)/3, (2/3))
+resultDiv = dividend / divider
 
-noteThirdContest = (requiredAverage * 3) - (noteFirstContest + noteSecondContest)
+result = math.log(0.76 * (originalTemp - boilingTemp) / (yolkMaxTemp - boilingTemp))
 
-print(f"The note required in the third contest to pass is: {noteThirdContest:.2f}.")
+seconds = resultDiv * result
+minuts = round(seconds / 60)
+
+print(f"The time needed in seconds to reach the maximum temperature is: {round(seconds)}.")
