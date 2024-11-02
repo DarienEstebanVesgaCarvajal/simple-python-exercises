ca0c47987c0344b7a278a2c70c018fcaf513923f
DarienEstebanVesgaCarvajal <182381184+DarienEstebanVesgaCarvajal@users.noreply.github.com>
2024-11-02 11:25:24 -0500

    circle

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-name = input("What's your name?")
-print(f"Hi!, dear {name}.")
+import math
+
+constantPi = math.pi
+radius = float(input("What's the radius of the circle?: "))
+
+print(f"The circunference of the circe is {radius * 2 * math.pi:.7f}.")
+print(f"The area of the circle is {math.pi * radius ** 2:.7f}.")
